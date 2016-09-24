592649591243b1dbf4970f90799f8a6946f4a6b8
iossenac <iossenac@sala309pc04.local>
2016-09-24 11:59:29 -0300

    Initial Commit

---
+++ EspeciesTableViewController.swift
@@ -0,0 +1,113 @@
+//
+//  EspeciesTableViewController.swift
+//  Be My Pet
+//
+//  Created by iossenac on 17/09/16.
+//  Copyright © 2016 Kassiane Mentz. All rights reserved.
+//
+
+import UIKit
+
+class EspeciesTableViewController: UITableViewController, UISplitViewControllerDelegate {
+    
+    // MARK: Split View Controller
+    
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        return true
+    }
+    
+    // MARK : - Model
+    let especies = [
+        (especie: "Cachorro"),
+        (especie: "Gato"),
+        (especie: "Passarinho"),
+        (especie: "Hamster")
+        
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        splitViewController?.delegate = self
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return especies.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath)
+        if let celula = cell as? EspecieTableViewCell {
+            let especie = especies[indexPath.row]
+            celula.configure(especie : especie)
+        }
+        
+        return cell
+    }
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        if(segue.identifier == "showDetail"){
+            if let indexPath = self.tableView.indexPathForSelectedRow{
+                let(especie) = especies[indexPath.row]
+                if let navcon = segue.destination as? UINavigationController {
+                    if let destino = navcon.visibleViewController as? EspecieDetailViewController {
+                        destino.especie = "Espécie: \(especie) "
+                    }
+                }
+            }
+        }
+    }
+
+}
